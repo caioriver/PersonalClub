dc52d6a9c394948e7cbd822a386349bdcc96c126
caioriver <43602376+caioriver@users.noreply.github.com>
2019-01-21 11:18:54 -0300

    Add files via upload

---
+++ styles.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components/native';
+
+export const Title = styled.Text`
+    color: #FFF;
+    font-size: 35;
+    padding-bottom: 80;
+    font-weight: bold;
+    font-family: 'Arial, Helvetica, sans-serif';
+`;
+
+export const Fundo = styled.View`
+    background: black;
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+    
+`;
+
+export const Termos = styled.Text`
+    text-align: center;
+    color: #FFF;
+    font-size: 15;
+`;
+
+// export const BotaoTexto = styled.Text`
+//     color: '#FFF';
+//     font-weight: 'bold';
+//     font-size: 30;
+// `;
